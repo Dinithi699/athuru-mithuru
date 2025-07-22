ebc6000e0bc0541daf8505192db7d22cda093687
Dinithi699 <109477578+Dinithi699@users.noreply.github.com>
2025-07-22 22:39:07 +0530

    Updated DysgraphiaGamePage.js

--- src/components/games/DysgraphiaGamePage.js
+++ src/components/games/DysgraphiaGamePage.js
@@ -659,8 +659,8 @@ const DysgraphiaGamePage = ({ onBack }) => {
             {/* Camera Preview */}
             {showCamera && (
               <div className="mb-6">
-                    
-                
+                    }
+                }
                 
                 
             <div className="relative bg-black rounded-lg overflow-hidden mb-4 mx-auto max-w-md">
